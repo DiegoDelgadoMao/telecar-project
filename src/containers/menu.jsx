e5615707b09950a6ed3e17229b2a0043444454a0
Diego Mauricio Delgado Espinosa <diegomcode@gmail.com>
2022-11-11 22:56:52 -0500

    agregando seccion de servicios

--- src/containers/menu.jsx
+++ src/containers/menu.jsx
@@ -1,40 +1,40 @@
-import React,{useState} from 'react';
-import {RiStore2Line,RiHandHeartLine,RiTeamLine} from "react-icons/ri"
+import React, { useState } from 'react';
+import { RiStore2Line, RiHandHeartLine, RiTeamLine } from "react-icons/ri"
 import '../styles/menu.scss'
 import logo from '../assets/images/logo.png';
 import MenuMobile from '../components/menu-mobile';
 import MenuButton from '../components/menu-bottom';
 const Menu = () => {
-    const [toggle,setToggle] = useState(false);
+    const [toggle, setToggle] = useState(false);
     const handleToggle = () => {
         setToggle(!toggle)
     }
-    return(
-        <div className ='menu-container'>
-        <div className ='menu-left'>
-            <img src={ logo }  alt="logo" />
-        </div>
-        <div className ='menu-right'>
-            <ul>
-                <li><a href="#">
-                      <RiStore2Line/>
-                      <p>Productos</p>
+    return (
+        <div className='menu-container'>
+            <div className='menu-left'>
+                <img src={logo} alt="logo" />
+            </div>
+            <div className='menu-right'>
+                <ul>
+                    <li><a href="#">
+                        <RiStore2Line />
+                        <p>Productos</p>
                     </a></li>
-                <li><a href="#">
-                <RiHandHeartLine/>
-                      <p>Servicios</p>
+                    <li><a href="#">
+                        <RiHandHeartLine />
+                        <p>Servicios</p>
                     </a></li>
-                <li><a href="#">
-                <RiTeamLine/>
-                      <p>Nosotros</p>
+                    <li><a href="#">
+                        <RiTeamLine />
+                        <p>Nosotros</p>
                     </a></li>
-            </ul>
+                </ul>
+            </div>
+            <div className="menu-mobile" onClick={handleToggle}>
+                <MenuButton />
+            </div>
+            {toggle && <MenuMobile />}
         </div>
-        <div className ="menu-mobile" onClick={handleToggle}>
-            <MenuButton/>
-        </div>
-        {toggle && <MenuMobile/>}
-    </div>
     )
 }
 
---
+++ src/pages/Services.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const Services = () => {
+	return (
+		<section className="services-section">
+			<h2>Que ofrecemos?</h2>
+			<ul>
+				<li>
+					Ventas de equipos y soluciones integrales en el sector
+					de las comunicaciones y seguridad electr,nica+
+				</li>
+				<li>
+					Servicio técnico instalaci,n equipos de radio equipos
+					de CCTV
+				</li>
+				<li>
+					Suministro de accesorios, baterías partes y piezas para
+					los diferentes equipos que ofrecemos, nuestros
+					servicios son prestados a nivel nacional.
+				</li>
+			</ul>
+		</section>
+	)
+}
