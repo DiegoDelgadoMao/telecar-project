54fe77ac18358f3652afd7260f4ca1337e391155
diego delgado mao <diegomcode@gmail.com>
2022-12-01 14:22:10 -0500

    renderizando productos en home (sin estilos)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,40 @@
-import React, { useEffect, useState} from 'react';
-import {getProduct,getAllproducts,getUpdateProducts} from '../firebase.js';
+import React, { useEffect, useState } from 'react';
+import { getProduct, getAllproducts, getUpdateProducts } from '../firebase.js';
 import '@styles/produc.scss'
 import OrderDetail from './ProductItem.jsx';
-// import Reactimg from '@images/pasted-image-0-2.png';
 
 const Home = () => {
-	 const [products, setProducts] = useState()
-	useEffect(()=>{
-		async function getData(){
-			const get = await getAllproducts();
-			get.forEach(doc => {
-				const data = doc.data();
-				console.log(data)})
+	const [products, setProducts] = useState([])
+
+	useEffect(() => {
+		async function getData() {
+			const query = await getAllproducts();
+			const mainProducts = []
+
+			query.forEach(doc => {
+				mainProducts.push(doc.data())
+				setProducts(mainProducts)
+			})
 		}
+		console.log(products)
 		getData();
-	},[])
+	}, [])
 
 	return (
 		<>
-			<h1>Hello world!</h1>
-			{}
+			<section className='container-products'>
+				{
+					products.map((item, index) => (
+						<div className='product' key={`${item.name}-${index}`}>
+							<h2>{item.name}</h2>
+							<p>{item.description}</p>
+							<h3>{item.cost}</h3>
+							<img src="{item.img}" alt="" />
+						</div>
+					))
+				}
+			</section>
+
 		</>
 	)
 }
