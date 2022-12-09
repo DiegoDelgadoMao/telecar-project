85325ba007d9641fcccaa463648c773d91522d0d
Sebastian <dev_sebasfc@outlook.es>
2022-12-08 22:17:35 -0500

    insert img

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -5,7 +5,7 @@ export const OrderDetail = ({ product }) => {
 	return (
 		<div className='bg-second text-fourth drop-shadow-second rounded-md'>
 			<div className='w-full h-2/4 grid place-items-center'>
-				<img src="https://www.telecar.com.co/wp-content/uploads/2019/08/RVA50.png" alt="image product" className='w-28 h-28' />
+				<img src={product.img} alt="image product" className='w-28 h-28' />
 			</div>
 			<div className='flex flex-col pl-6 gap-1 mt-2 h-2/4'>
 				<p className='text-xl font-black'>${product.cost}</p>
